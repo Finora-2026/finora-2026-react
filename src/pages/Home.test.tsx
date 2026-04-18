cdc829c2efbb2584e217d8ba8983d3dfdbedd8e8
Bellamy Phan <bellamyphan@icloud.com>
2026-04-18 10:46:48 -0500

    FI-127: add unit tests to the Home page

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -14,4 +14,24 @@ describe("Home page", () => {
         expect(screen.getByText("Bellamy Phan")).toBeInTheDocument();
         expect(document.title).toBe("Bellamy Phan | Home");
     });
+    
+    it("renders bottom documentation box", () => {
+        render(
+          <MemoryRouter>
+              <Home />
+          </MemoryRouter>
+        );
+        
+        expect(screen.getByText("Documentation")).toBeInTheDocument();
+    });
+    
+    it("renders bottom connect-with-us box", () => {
+        render(
+          <MemoryRouter>
+              <Home />
+          </MemoryRouter>
+        );
+        
+        expect(screen.getByText("Connect with us")).toBeInTheDocument();
+    });
 });
