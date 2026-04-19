050ef228d1ce481944c996dab42f55500cdea3c8
Bellamy Phan <bellamyphan@icloud.com>
2026-04-19 02:08:03 -0500

    FI-134: apply bootstrap to investmentCalculator.Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,22 @@
-import logo from '../assets/investment-calculator-logo.png'
-import styles from './Header.module.css'
+import logo from '../assets/investment-calculator-logo.png';
 
 export default function Header() {
-    return <header className={styles.header}>
-        <img src={logo} alt="Logo showing a money bag"></img>
-        <h1>Investment Calculator</h1>
-    </header>
+    return (
+      <header className="text-center my-5">
+          
+          <img
+            src={logo}
+            alt="Logo showing a money bag"
+            className="d-block mx-auto mb-3"
+            style={{
+                width: "5rem",
+                height: "5rem",
+                objectFit: "contain"
+            }}
+          />
+          
+          <h1 className="fs-3">Investment Calculator</h1>
+      
+      </header>
+    );
 }
