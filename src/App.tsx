40ae85e059b563962af5fdaa5df03833b73e3820
bellamyphan <bellamyphan@icloud.com>
2026-05-07 00:24:30 -0500

    FI-158: move page files to their folders

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import {Route, Routes} from "react-router-dom";
 
-import Home from "./pages/Home.tsx";
+import Home from "./pages/home/Home.tsx";
 import Portfolio from "./pages/portfolio/Portfolio.tsx";
 
 import './App.css'
 import Navbar from "./components/NavBar/NavBar.tsx";
-import FinoraLogin from "./pages/FinoraLogin.tsx";
-import InvestmentCalculator from "./pages/InvestmentCalculator.tsx";
+import InvestmentCalculator from "./pages/investment/InvestmentCalculator.tsx";
 import Movie from "./pages/movie/Movie.tsx";
 import MovieDetail from "./pages/movie/MovieDetail.tsx";
 import NotFound from "./pages/notFound/NotFound.tsx";
 import SignUp from "./pages/AuthPages/SignUp.tsx";
 import SignIn from "./pages/AuthPages/SignIn.tsx";
 import {ToastProvider} from "./components/ToastProvider/ToastProvider.tsx";
+import FinoraPage from "./pages/finora/FinoraPage.tsx";
 
 export default function App() {
   return (
@@ -25,7 +25,7 @@ export default function App() {
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/finora-login" element={<FinoraLogin />} />
+        <Route path="/finora" element={<FinoraPage />} />
         <Route path="/investment-calculator" element={<InvestmentCalculator />} />
         <Route path="/movies" element={<Movie />} />
         <Route path="/movies/:imdbId" element={<MovieDetail />} />
