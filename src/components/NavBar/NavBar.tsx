8184b8ad04f067fc2e9269929583936187b0fdde
bellamyphan <bellamyphan@icloud.com>
2026-05-06 19:51:11 -0500

    FI-157: create module scss file for NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { authService } from "../../utils/authService.ts";
-import "./NavBar.css";
+import styles from "./NavBar.module.scss";
 
 export default function Navbar() {
   const location = useLocation();
@@ -29,22 +29,23 @@ export default function Navbar() {
   };
 
   return (
-    <nav className="navbar">
-      <div className="nav-links">
-        <Link className={location.pathname === "/" ? "active" : ""} to="/">
+    <nav className={styles.navbar}>
+      <div className={styles.navLinks}>
+        <Link className={location.pathname === "/" ? styles.active : ""} to="/">
           Home
         </Link>
 
-        <Link className={location.pathname === "/portfolio" ? "active" : ""} to="/portfolio">
+
+        <Link className={location.pathname === "/portfolio" ? styles.active : ""} to="/portfolio">
           Portfolio
         </Link>
 
-        <Link className={location.pathname === "/finora-login" ? "active" : ""} to="/finora-login">
+        <Link className={location.pathname === "/finora-login" ? styles.active : ""} to="/finora-login">
           Finora
         </Link>
       </div>
 
-      <div className="nav-name">
+      <div className={styles.navName}>
         {isLoggedIn && user ? (
           <button onClick={handleLogout}>
             {user.email} (Log Out)
