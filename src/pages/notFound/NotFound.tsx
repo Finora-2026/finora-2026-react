997dcb98f73c663bb64ceb9b568180b2dd90c6d7
bellamyphan <bellamyphan@icloud.com>
2026-05-09 02:15:46 -0500

    FI-201: notfound page > add go back button

--- src/pages/notFound/NotFound.tsx
+++ src/pages/notFound/NotFound.tsx
@@ -1,26 +1,35 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./NotFound.module.scss";
-import {useEffect} from "react";
+import { useEffect } from "react";
 
 export default function NotFound() {
-
   const navigate = useNavigate();
 
   useEffect(() => {
-    document.title = 'Bellamy Phan | Error Page'
-  }, [])
+    document.title = "Bellamy Phan | Error Page";
+  }, []);
 
   return (
     <div className={styles.container}>
       <h1>404 - Page Not Found</h1>
 
-      <button
-        className={styles.counter}
-        onClick={() => navigate("/")}
-        style={{ marginTop: "20px" }}
-      >
-        Go Back Home
-      </button>
+      <div className={styles.buttonGroup}>
+
+        <button
+          className={styles.counter}
+          onClick={() => navigate(-1)}
+        >
+          Previous Page
+        </button>
+
+        <button
+          className={styles.counter}
+          onClick={() => navigate("/")}
+        >
+          Go Back Home
+        </button>
+
+      </div>
     </div>
   );
 }
