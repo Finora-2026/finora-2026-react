1c92d537d4ad2de98b41b89ba13399402b728e34
Bellamy Phan <55559722+bellamyphan@users.noreply.github.com>
2026-05-15 12:23:10 -0500

    FI-304: Build correct page title for Finora tab (#65)

--- src/pages/finora/FinoraPage.tsx
+++ src/pages/finora/FinoraPage.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import NotFound from "../notFound/NotFound.tsx";
 import FinoraMenu from "../finoraMenu/FinoraMenu.tsx";
 import FinoraNavBar from "../../components/NavBar/NavBarFinora.tsx";
@@ -9,9 +9,22 @@ import TransactionRoutes from "../../route/TransactionRoutes.tsx";
 const FinoraAnalytics = () => <div className="p-10">Finora Analytics Content</div>;
 
 export default function FinoraPage() {
+  
+  const location = useLocation();
+  
   useEffect(() => {
-    document.title = 'Bellamy Phan | Finora';
-  }, []);
+    const path = location.pathname;
+    let subtitle = "";
+    // Determine the subtitle based on the path
+    if (path.includes("/accounts")) {
+      subtitle = " | Accounts";
+    } else if (path.includes("/transactions")) {
+      subtitle = " | Transactions";
+    } else if (path.includes("/analytics")) {
+      subtitle = " | Analytics";
+    }
+    document.title = `Finora${subtitle}`;
+  }, [location]); // Re-runs every time the URL changes
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
